a6dc8706e1ba48c323e6dbe7f100c1c9428e2efa
Alex Noble <swinkid@googlemail.com>
2017-02-12 20:58:32 +0000

    Added realtime functionality

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,5 +1,6 @@
 module.exports = function (io){
     setInterval(function () {
+        // TODO: Maybe call API call here instead of every client?
         io.emit('update', '');
-    }, 1000);
+    }, 500);
 };
