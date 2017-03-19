cabac0ea97794a0989feb9eaa704910368d51fe0
Alex Noble <swinkid@googlemail.com>
2017-03-19 18:44:55 +0000

    Fixing SSL issues

--- app.js
+++ app.js
@@ -44,6 +44,16 @@ app.use(session({
     })
 }));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain());
+
 app.set('trust proxy', 1);
 
 app.use(passport.initialize());
